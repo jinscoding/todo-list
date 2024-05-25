e9b2cf1b6c111edd717203ac9ed43964953a573d
hojin <hojin9901@naver.com>
2024-05-25 21:16:41 +0900

    Add Clock

---
+++ src/Clock.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const Clock :  React.FC = () => {
+    const [time, settime] = useState(new Date());
+
+    setInterval(()=>{
+        settime(new Date());
+    }, 1000);
+    
+    return (
+        <div>
+            현재 시간 : {time.toLocaleTimeString()}
+        </div>
+    )
+}
+
+export default Clock;
